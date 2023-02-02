37a40903e4905a7c0502f0e4f132b25cd9e5f125
monumentalsquid <samuel@samuel-crawford.com>
2023-02-02 09:50:08 -0500

    dotenv support and refactor note model

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
 const app = express();
 
 app.use(express.json());
@@ -39,18 +41,26 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
+  // .catch((error) => {
+  //   response.status(404).end();
+  // });
+
+  // const id = Number(request.params.id);
+  // const note = notes.find((note) => note.id === id);
+  // if (note) {
+  //   response.json(note);
+  // } else {
+  //   response.status(404).end();
+  // }
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -68,22 +78,21 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    console.log(response);
+    response.json(savedNote); //data sent back in response is formatted version created automatically with toJSON method
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -92,7 +101,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
